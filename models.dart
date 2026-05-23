9ea6967d329f4617041b7b10e74452713caf08d7
gerb768-rgb <gerb768@gmail.com>
2026-05-23 13:28:28 +0300

    Update models.dart

--- models.dart
+++ models.dart
@@ -1,8 +1,1 @@
-// منطق بسيط لاكتشاف الأدوية التي تنتهي خلال 3 أشهر
-if (isExpiringSoon(med['expiry_date'])) {
-   return ListTile(
-     title: Text(med['name']),
-     tileColor: Colors.red[100], // تلوين الدواء باللون الأحمر للتنبيه
-     subtitle: Text("انتبه! الدواء يقترب من انتهاء الصلاحية"),
-   );
-}
+Text("السعر: ${med['price']} ج.س")
