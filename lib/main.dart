940f7fe2dd8e7d597194f3df025e4968702605df
gerb768-rgb <gerb768@gmail.com>
2026-05-23 12:41:10 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
-class Medication {
-  final String name;
-  final double price;
-
-  Medication(this.name, this.price);
-}
-
-// هذه هي قائمة الأدوية الفعلية في نظامك
-final List<Medication> pharmacyItems = [
-  Medication("بندول", 15.5),
-  Medication("أموكسيسيلين", 45.0),
-  Medication("فيتامين C", 20.0),
-];
+// في ملف main.dart، أضف هذا الجزء في أسفل الصفحة (Footer)
+const Padding(
+  padding: EdgeInsets.all(8.0),
+  child: Text(
+    "تصميم وتطوير: عمر يوسف البطحاني | © 2026 جميع الحقوق محفوظة",
+    style: TextStyle(fontSize: 12, color: Colors.grey),
+  ),
+),
