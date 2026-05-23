b4097c05076bbf178a027f7f5fb3564d26497260
gerb768-rgb <gerb768@gmail.com>
2026-05-23 12:53:33 +0300

    Update database_helper.dart

--- database_helper.dart
+++ database_helper.dart
@@ -1,1 +1,34 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('ghasaq_system.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE medications (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        price REAL NOT NULL,
+        stock_quantity INTEGER NOT NULL,
+        expiry_date TEXT NOT NULL,
+        barcode TEXT UNIQUE
+      )
+    ''');
+  }
+}
